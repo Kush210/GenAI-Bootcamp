8079f70b58e430938ab3c5c36c8acade8ce126e1
shubham-jhajhria <jhajhariashubham9@gmail.com>
2024-12-24 22:37:41 +0530

    added pinecone and TODO completions

--- Codes/Week_1/Day_1/using_langChain.py
+++ Codes/Week_1/Day_1/using_langChain.py
@@ -1,26 +1,35 @@
-#TODO: Provide option to enable streaming in this
-
 # Ensure Ollama is installed and running at http://localhost:11434
 # To run local ollama server type: "ollama serve" in terminal
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     totalAvailableQues = 3
     currentQues = 0
 
     template = """Question:{question}
-    
+
     Answer: """
+
+    stream = False # for streaming the response
 
     model = ChatOllama(model="llama3.2")
     prompt = ChatPromptTemplate.from_template(template=template)
 
     chain = prompt | model
 
-    while currentQues<totalAvailableQues:
+    while currentQues < totalAvailableQues:
         que = str(input("Question: "))
-        response = chain.invoke({"question":que})
-        print(response.content)
-        currentQues+=1
+        if stream:
+            response = chain.stream({"question": que})
+            for chunk in response:
+                print(chunk.content,end='',flush=True)
+            print('\n')
+            currentQues+=1
+        else:
+            response = chain.invoke({"question": que})
+            print(response.content, end="", flush=True)  
+            print() 
+            currentQues += 1
+
