c8c81793b7eeb0d46ec0cd55bf73f25130333ce2
Kush Suryavanshi <kush2101999@gmail.com>
2025-02-08 12:56:40 -0500

    week_2 capstone

---
+++ Codes/Week_3/Day_2/Audio_Recorder_in_Sreamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from audiorecorder import audiorecorder
+
+
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Click to stop recording")
+
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.export().read())
